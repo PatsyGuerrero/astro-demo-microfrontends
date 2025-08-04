aed938ba7bbd1a2b07d7c15e7bac30d9c3f442e7
Patsy Guerrero <pguerrero3@bancoguayaquil.com>
2025-08-04 16:55:57 -0500

    creacion de archivos

--- content/astro.config.mjs
+++ content/astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import moduleFederation from "astro-module-federation";
 import react from '@astrojs/react';
+import dotenv from 'dotenv';
 
-const remoteBaseUrl = process.env.REMOTE_LIBRARY_URL ?? 'http://localhost:5143' ;
+dotenv.config(); // 👈 esto permite leer process.env en tiempo de build
+const remoteBaseUrl = process.env.REMOTE_LIBRARY_URL ?? 'http://localhost:4173' ;
+
+console.log('base', remoteBaseUrl);
 
 // https://astro.build/config
 export default defineConfig({
